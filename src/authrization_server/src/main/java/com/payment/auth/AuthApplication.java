804fa4530f45f546b92dc50691c4475afcbfb4a6
Seongmun-Hong <hsm63746244@gmail.com>
2019-05-28 19:18:33 +0900

    change : init project 변경

--- src/authrization_server/src/main/java/com/payment/auth/AuthApplication.java
+++ src/authrization_server/src/main/java/com/payment/auth/AuthApplication.java
@@ -1,13 +1,13 @@
-package com.payment.auth.authorization_server;
+package com.payment.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthorizationServerApplication {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationServerApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 
 }
--- src/authrization_server/src/test/java/com/payment/auth/AuthApplicationTests.java
+++ src/authrization_server/src/test/java/com/payment/auth/AuthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.payment.auth.authorization_server;
+package com.payment.auth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AuthorizationServerApplicationTests {
+public class AuthApplicationTests {
 
     @Test
     public void contextLoads() {
