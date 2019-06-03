b4eab8320991ada1b6caf1ec35b78d4bc4479d7e
Seongmun-Hong <hsm63746244@gmail.com>
2019-06-03 12:29:00 +0900

    change : merchant server 서버 새로 생성

--- src/merchant_server/src/main/java/com/payment/merchant/repository/OrderRedisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.payment.merchant.repository;
-
-import com.payment.merchant.model.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRedisRepository extends CrudRepository<Order, String> {
-}
--- src/merchant_server/src/main/java/com/payment/merchant/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.payment.merchant.service;
-
-public interface OrderService {
-
-    long reserve(String encodedOrderName, int amount, int count);
-
-    void approval(String redisKey, String payId);
-
-}
